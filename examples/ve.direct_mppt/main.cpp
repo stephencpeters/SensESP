11a4b181b18d2ea46cf26fd17e35d6a0ae2236af
stephencpeters <80661869+stephencpeters@users.noreply.github.com>
2021-03-25 23:24:14 -0400

    Update main.cpp

--- examples/ve.direct_mppt/main.cpp
+++ examples/ve.direct_mppt/main.cpp
@@ -9,6 +9,7 @@ ReactESP app([]() {
   sensesp_app = builder.set_standard_sensors(ALL) //or, IP_ADDRESS, UPTIME, NONE
                     ->get_app();
 
+#define SERIAL_DEBUG_DISABLED = True  // Also uncomment debug lines in parser and helper
 #ifndef SERIAL_DEBUG_DISABLED
   SetupSerialDebug(115200);
 #endif
@@ -24,10 +25,10 @@ ReactESP app([]() {
 #elif defined(ESP32)
 
 //  Assign pins for ESP32 serial communication.
-//  The first 2 are connected to hardware UARTs
-//  The next 4 are software UARTs.
+//  Uncomment the ones you plan to use.
+//  The first 3 are connected to hardware UARTs
+//  The next 3 are software UARTs.
 //  Unknown if more could be defined.
-//  The default pins are 27, 16, 26, 17, 25, 33
 //
 //  Define hardware UARTs
 //  Use these first if you have up to two ve.direct MPPTs
@@ -37,29 +38,37 @@ ReactESP app([]() {
   HardwareSerial* serialA = &Serial1;
   serialA->begin(19200, SERIAL_8N1, pinA, -1);
 
-  uint8_t pinB = 16;   //  Keep UART2 on default pin 16
-  HardwareSerial* serialB = &Serial2;
-  serialB->begin(19200, SERIAL_8N1, pinB, -1);
+  // uint8_t pinB = 16;   //  Keep UART2 on default pin 16
+  // HardwareSerial* serialB = &Serial2;
+  // serialB->begin(19200, SERIAL_8N1, pinB, -1);
 
-  //Note that this UART defaults to the USB chip on many dev kits
-  uint8_t pinC = 26;  // Reassign UART0 to pin 26
-  HardwareSerial* serialC = &Serial;
-  serialC->begin(19200, SERIAL_8N1, pinC, -1);
+//  Note that UART0 defaults to the USB chip on many dev kits
+//  Seems to work fine though you must reassign the pin.
+  // uint8_t pinC = 26;  // Reassign UART0 to pin 26
+  // HardwareSerial* serialC = &Serial;
+  // serialC->begin(19200, SERIAL_8N1, pinC, -1);
  
-//  Define the rest of the serial ports in software
+//  Define the rest of the serial ports using software serial
 //  These are labeled 4, 5, 6 in the Signal K path names
 
-  uint8_t pinD = 17;  // Use software serial on pin 17
-  SoftwareSerial* serialD = new SoftwareSerial(pinD, -1, false);
-  serialD->begin(19200, SWSERIAL_8N1);
+  // uint8_t pinD = 17;  // Use software serial on pin 17
+  // SoftwareSerial* serialD = new SoftwareSerial(pinD, -1, false);
+  // serialD->begin(19200, SWSERIAL_8N1);
 
-  uint8_t pinE = 25;  // Use software serial on pin 25
-  SoftwareSerial* serialE = new SoftwareSerial(pinE, -1, false);
-  serialE->begin(19200, SWSERIAL_8N1);
+  // uint8_t pinE = 25;  // Use software serial on pin 25
+  // SoftwareSerial* serialE = new SoftwareSerial(pinE, -1, false);
+  // serialE->begin(19200, SWSERIAL_8N1);
 
-  uint8_t pinF = 33;  // Use software serial on pin 33
-  SoftwareSerial* serialF = new SoftwareSerial(pinF, -1, false);
-  serialF->begin(19200, SWSERIAL_8N1);
+  // uint8_t pinF = 33;  // Use software serial on pin 33
+  // SoftwareSerial* serialF = new SoftwareSerial(pinF, -1, false);
+  // serialF->begin(19200, SWSERIAL_8N1);
+
+  // Enable level shifter
+  // Wire to OE pin on 8 channel single chip TXS0108E
+  uint8_t pinLevelShifterPower = 23;
+  pinMode(pinLevelShifterPower, OUTPUT);
+  digitalWrite(pinLevelShifterPower, HIGH);
+
 
 #endif
 
@@ -72,18 +81,20 @@ ReactESP app([]() {
 
 
   auto* charger1 = setup_vedirect(serialA, 1);
-   // String("Davits").value(
-  //   new SKOutputString("electrical.solar.charger1.name", "/ve.direct.1/chargerNamePath"));
+  // String("Davits")->(
+  // new SKOutputString("electrical.solar.charger1.name", "/ve.direct.1/chargerNamePath"));
+  //    Marco - this^^ is where I was trying to hard code a name
+  //    so it could be displayed etc.
 
-  auto* charger2 = setup_vedirect(serialB, 2);
+  // auto* charger2 = setup_vedirect(serialB, 2);
 
-  auto* charger3 = setup_vedirect(serialC, 3);
+  // auto* charger3 = setup_vedirect(serialC, 3);
 
-  auto* charger4 = setup_vedirect(serialD, 4);
+  // auto* charger4 = setup_vedirect(serialD, 4);
 
-  auto* charger5 = setup_vedirect(serialE, 5);
+  // auto* charger5 = setup_vedirect(serialE, 5);
 
-  auto* charger6 = setup_vedirect(serialF, 6);
+  // auto* charger6 = setup_vedirect(serialF, 6);
 
   sensesp_app->enable();
 });
